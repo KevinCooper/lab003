3c4870a5774b078e0a16ef8b633a2aabdec2a6dd
Kevin Cooper <boo2atfoo@gmail.com>
2014-02-25 10:26:10 -0700

    Added in the non generic DFFs to allow the main character gen to look much nicer and make the pipelining much simpler.  Should have used geenerics so I would only need to make one.

--- dff2.vhd
+++ dff2.vhd
@@ -4,22 +4,22 @@ use IEEE.NUMERIC_STD.ALL;
 library UNISIM;
 use UNISIM.VComponents.all;
 
-entity dff is
+entity dff2 is
     Port ( clk : in  STD_LOGIC;
            reset : in  STD_LOGIC;
-           d : in  STD_LOGIC;
-           q : out  STD_LOGIC);
-end dff;
+           d : in  STD_LOGIC_VECTOR(2 downto 0);
+           q : out  STD_LOGIC_VECTOR(2 downto 0));
+end dff2;
 
-architecture cooper of dff is
-signal data: std_logic;
+architecture cooper of dff2 is
+signal data: std_logic_vector(2 downto 0);
 begin
 
 process (clk, reset) is
 begin
 	data <= data;
 	if(reset='1') then
-		data <= '0';
+		data <= "000";
 	elsif(rising_edge(clk)) then
 		data <= d;
 	end if;
@@ -28,4 +28,3 @@ end process;
 q<= data;
 
 end cooper;
-
---
+++ dff3.vhd
@@ -0,0 +1,30 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity dff3 is
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           d : in  STD_LOGIC_VECTOR(3 downto 0);
+           q : out  STD_LOGIC_VECTOR(3 downto 0));
+end dff3;
+
+architecture cooper of dff3 is
+signal data: std_logic_vector(3 downto 0);
+begin
+
+process (clk, reset) is
+begin
+	data <= data;
+	if(reset='1') then
+		data <= "0000";
+	elsif(rising_edge(clk)) then
+		data <= d;
+	end if;
+end process;
+
+q<= data;
+
+end cooper;
